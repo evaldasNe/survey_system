c07e3d7b9db3b2a3c6208caed5dada0bb61972a0
evaldasNe <neevaldas@gmail.com>
2019-12-20 02:05:45 +0200

    user can attend survey

--- src/Controller/AttendSurveyController.php
+++ src/Controller/AttendSurveyController.php
@@ -2,83 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\AnswerOption;
 use App\Entity\AttendSurvey;
 use App\Entity\Survey;
 use App\Form\AttendSurveyType;
 use App\Repository\AttendSurveyRepository;
+use Doctrine\Common\Collections\Collection;
+use http\Client\Curl\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/attend/survey")
+ * @Route("user/attend/survey")
  */
 class AttendSurveyController extends AbstractController
 {
-    /**
-     * @Route("/", name="attend_survey_index", methods={"GET"})
-     */
-    public function index(AttendSurveyRepository $attendSurveyRepository): Response
-    {
-        return $this->render('attend_survey/index.html.twig', [
-            'attend_surveys' => $attendSurveyRepository->findAll(),
-        ]);
-    }
 
     /**
-     * @Route("/attend_survey/{id}", name="attend_survey_new", methods={"GET","POST"})
+     * @Route("/{id}", name="attend_survey", methods={"GET","POST"})
      */
-    public function new(Request $request, Survey $survey): Response
+    public function new(Request $request, Survey $survey, AttendSurveyRepository $attendSurveyRepository): Response
     {
+        if ($attendSurveyRepository->findBy(['user' => $this->getUser(), 'survey' => $survey]) != null){
+            return $this->redirectToRoute('home_page');
+        }
         $attendSurvey = new AttendSurvey();
         $attendSurvey->setSurvey($survey);
         $attendSurvey->setUser($this->getUser());
-        $form = $this->createForm(AttendSurveyType::class, $attendSurvey);
+
+        $form = $this->createFormBuilder();
+
+        foreach ($survey->getQuestions() as $question){
+            $form->add('answers'.$question->getId(), EntityType::class, [
+                'class' => AnswerOption::class,
+                'label' => ucfirst($question->getTitle()),
+                'choice_label' => 'answer',
+                'choices' => $question->getAnswerOptions(),
+            ]);
+        }
+        $form = $form->getForm();
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+            foreach ($data as $answer){
+                $attendSurvey->getAnswers()->add($answer);
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($attendSurvey);
             $entityManager->flush();
 
-            return $this->redirectToRoute('attend_survey_index');
+            return $this->redirectToRoute('home_page');
         }
 
         return $this->render('attend_survey/new.html.twig', [
             'attend_survey' => $attendSurvey,
             'form' => $form->createView(),
             'survey' => $survey,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="attend_survey_show", methods={"GET"})
-     */
-    public function show(AttendSurvey $attendSurvey): Response
-    {
-        return $this->render('attend_survey/show.html.twig', [
-            'attend_survey' => $attendSurvey,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="attend_survey_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, AttendSurvey $attendSurvey): Response
-    {
-        $form = $this->createForm(AttendSurveyType::class, $attendSurvey);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('attend_survey_index');
-        }
-
-        return $this->render('attend_survey/edit.html.twig', [
-            'attend_survey' => $attendSurvey,
-            'form' => $form->createView(),
         ]);
     }
 
@@ -93,6 +79,6 @@ class AttendSurveyController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('attend_survey_index');
+        return $this->redirectToRoute('home_page');
     }
 }
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AttendSurveyRepository;
 use App\Repository\SurveyRepository;
+use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,18 @@ class HomePageController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index(SurveyRepository $surveyRepository)
+    public function index(SurveyRepository $surveyRepository, AttendSurveyRepository $attendSurveyRepository)
     {
+        $attended_surveys = array();
+        if ($this->getUser() != null){
+            $attended = $attendSurveyRepository->findBy(['user' => $this->getUser()]);
+            foreach ($attended as $item){
+                array_push($attended_surveys, $item->getSurvey());
+            }
+        }
         return $this->render('home_page/index.html.twig', [
             'surveys' => $surveyRepository->findAll(),
+            'attended_surveys' => $attended_surveys,
         ]);
     }
 }
